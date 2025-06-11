489aa079afe8437f99b669e52fb133e2f99986e7
HyeonProG <guswo3443@naver.com>
2025-06-11 14:14:55 +0900

    refactor: 팔로우 리스트 조회 에러 핸들링 추가

--- src/main/java/com/vybz/follow_service/follow/application/FollowServiceImpl.java
+++ src/main/java/com/vybz/follow_service/follow/application/FollowServiceImpl.java
@@ -73,6 +73,10 @@ public class FollowServiceImpl implements FollowService {
      */
     @Override
     public CursorPageUtil<ResponseUserFollowingDto, String> getFollowingByUserUuid(String userUuid, String lastId, Integer pageSize, Integer page) {
+        if (!followRepository.existsByUserUuid(userUuid)) {
+            throw new BaseException(BaseResponseStatus.NO_EXIST_USER);
+        }
+
         List<Follow> follows = followRepository.findFollowingByCursor(userUuid, lastId, pageSize + 1, page);
         boolean hasNext = follows.size() > pageSize;
         if (hasNext) {
@@ -106,6 +110,10 @@ public class FollowServiceImpl implements FollowService {
      */
     @Override
     public CursorPageUtil<ResponseBuskerFollowerDto, String> getFollowerByBuskerUuid(String buskerUuid, String lastId, Integer pageSize, Integer page) {
+        if (!followRepository.existsByBuskerUuid(buskerUuid)) {
+            throw new BaseException(BaseResponseStatus.NO_EXIST_BUSKER);
+        }
+
         List<Follow> follows = followRepository.findFollowerByCursor(buskerUuid, lastId, pageSize + 1, page);
         boolean hasNext = follows.size() > pageSize;
         if (hasNext) {
